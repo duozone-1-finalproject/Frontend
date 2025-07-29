752a7e9b28fce3f63281d733c74f94d7008c5576
JuKoNuNu <127651508+JuKoNuNu@users.noreply.github.com>
2025-07-29 10:27:47 +0900

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
+import RegisterPage from "./pages/RegisterPage";
+import OAuthSuccessPage from './pages/OAuthSuccessPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} /> 
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/Register" element={<RegisterPage />} />
+        <Route path="/oauth-success" element={<OAuthSuccessPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
