f02b39214b53cdcc16c584dfcc40503da539627e
JuKoNuNu <127651508+JuKoNuNu@users.noreply.github.com>
2025-09-13 14:03:54 +0900

    123

--- src/components/dart-viewer/document-viewer.tsx
+++ src/components/dart-viewer/document-viewer.tsx
@@ -29,7 +29,7 @@ export function DocumentViewer() {
     handleCreateNewVersion,
     handleDeleteEditingVersion,
     handleSwitchVersion,
-  } = useDocumentViewer(123)
+  } = useDocumentViewer(123456)
 
   return (
     <div className="h-screen flex flex-col bg-white">
@@ -144,7 +144,7 @@ export function DocumentViewer() {
             </div>
           ) : (
             <DocumentContent
-              userId={123}
+              userId={123456}
               htmlContent={currentSectionHTML}
               sectionId={selectedSection}
               sectionName={currentSection?.sectionName}
