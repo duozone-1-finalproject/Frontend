10323e23607876c0b6cd7b44a13265fd14345ce0
wanter9091 <dhqudwo2976@naver.com>
2025-09-13 14:21:58 +0900

    chore : 개인 모드 인덱스

--- src/components/dart-viewer/document-viewer.tsx
+++ src/components/dart-viewer/document-viewer.tsx
@@ -29,7 +29,7 @@ export function DocumentViewer() {
     handleCreateNewVersion,
     handleDeleteEditingVersion,
     handleSwitchVersion,
-  } = useDocumentViewer(123)
+  } = useDocumentViewer(321321)
 
   return (
     <div className="h-screen flex flex-col bg-white">
@@ -144,7 +144,7 @@ export function DocumentViewer() {
             </div>
           ) : (
             <DocumentContent
-              userId={123}
+              userId={321321}
               htmlContent={currentSectionHTML}
               sectionId={selectedSection}
               sectionName={currentSection?.sectionName}
