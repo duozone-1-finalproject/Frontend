566adf6ba52805fb2afb1757f0a1bf59f4febea0
SeoheeLim <135564947+SeoheeLim@users.noreply.github.com>
2025-09-16 22:05:47 +0900

    fix: 6

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -72,7 +72,7 @@ export const useLogin = () => {
   };
 
   const redirectToOAuth = (provider: OAuthProvider) => {
-    window.location.href = `${axios.defaults.baseURL}/oauth2/authorization/${provider}`;
+    window.location.href = `http://k8s-default-ingress-164f943143-393493025.ap-northeast-2.elb.amazonaws.com/backend/oauth2/authorization/${provider}`;
   };
 
   const handleRegisterClick = () => {
