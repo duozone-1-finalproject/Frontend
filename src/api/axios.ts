ce003cc6d9e32765527b9b87cd2e98c264cb3d28
SeoheeLim <135564947+SeoheeLim@users.noreply.github.com>
2025-09-16 14:32:47 +0900

    fix: 백엔드 링크(#22)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const TOKEN_KEY = "accessToken";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://k8s-default-backend-e5561f3a96-bc5919da2e57a425.elb.ap-northeast-2.amazonaws.com",
   headers: {
     "Content-Type": "application/json",
   },
