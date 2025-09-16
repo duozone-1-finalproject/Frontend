fbb8067492d9fc533aa1ec7aaa0a2e2bd3ca5e7d
SeoheeLim <135564947+SeoheeLim@users.noreply.github.com>
2025-09-16 14:20:21 +0900

    fix: 로컬호스트 지움(#22)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const TOKEN_KEY = "accessToken";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
