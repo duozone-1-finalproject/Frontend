abf63a55b5a54c399c0ff643280515262a29791c
SeoheeLim <135564947+SeoheeLim@users.noreply.github.com>
2025-09-16 21:55:56 +0900

    fix: 6

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const TOKEN_KEY = "accessToken";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://k8s-default-backend-e5561f3a96-bc5919da2e57a425.elb.ap-northeast-2.amazonaws.com",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "k8s-default-ingress-164f943143-393493025.ap-northeast-2.elb.amazonaws.com/backend",
   headers: {
     "Content-Type": "application/json",
   },
