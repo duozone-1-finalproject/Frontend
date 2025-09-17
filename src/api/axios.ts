3101a15d7cfa2aeab18cf3117dd394805c699086
SeoheeLim <135564947+SeoheeLim@users.noreply.github.com>
2025-09-17 20:54:07 +0900

    fix: 9/17-15

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,18 +1,13 @@
 import axios from "axios";
 
-// baseURL이 비어있으면 기본값 사용 (중요!)
-const baseURL = (process.env.REACT_APP_API_BASE_URL ||
-  "http://k8s-default-ingress-164f943143-1841556789.ap-northeast-2.elb.amazonaws.com/backend")
-  .replace(/\/$/, ""); // 끝 슬래시 제거
-
-console.log("🔧 Axios baseURL:", baseURL);
-console.log("🔧 Environment:", process.env.REACT_APP_API_BASE_URL);
+const BASE_URL = process.env.REACT_APP_API_BASE_URL?.replace(/\/$/, "")
+                 || "http://k8s-default-ingress-164f943143-1841556789.ap-northeast-2.elb.amazonaws.com/backend";
 
 const instance = axios.create({
-  baseURL,
+  baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
-  timeout: 15000,
 });
+
 
 export default instance;
