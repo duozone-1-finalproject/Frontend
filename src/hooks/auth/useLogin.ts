f0ad7652d4dc8cfb88bb762f1d489d396abdb1a5
SeoheeLim <135564947+SeoheeLim@users.noreply.github.com>
2025-09-17 19:38:40 +0900

    fix: 9/17-7

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -72,7 +72,8 @@ export const useLogin = () => {
   };
 
   const redirectToOAuth = (provider: OAuthProvider) => {
-    window.location.href = `http://k8s-default-ingress-164f943143-1841556789.ap-northeast-2.elb.amazonaws.com/backend/oauth2/authorization/${provider}`;
+    const backendUrl = process.env.REACT_APP_API_BASE_URL || 'http://k8s-default-ingress-164f943143-1841556789.ap-northeast-2.elb.amazonaws.com/backend';
+    window.location.href = `${backendUrl}/oauth2/authorization/${provider}`;
   };
 
   const handleRegisterClick = () => {
